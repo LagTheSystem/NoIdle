77306315256d0e81ab992059959a4197a05fde09
LagTheSystem <lagthesystem@duck.com>
2024-03-19 10:24:58 -0500

    Update NoIdle.py

--- NoIdle.py
+++ NoIdle.py
@@ -1,11 +1,13 @@
-import keyboard
+import keyboard as kb
 from time import sleep
 
-count = 0
-print("NoIdle 1.2.0")
+print("NoIdle 1.3-b1")
 print("By LagTheSystem\n")
 print("To stop macro, close this window!")
-while count == 0:
-    sleep(1)
-    keyboard.press_and_release('w')
+sleep(1)
+while true:
+    kb.KEY_DOWN('W')
+    sleep(0.5)
+    kb.KEY_UP('W')
+    sleep(0.5)
 
