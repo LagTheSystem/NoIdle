a938bff674cb6f0ab182f53fb25b5c60cd6cbeae
LagTheSystem <lagthesystem@duck.com>
2024-03-19 10:12:33 -0500

    Update NoIdle.py

--- NoIdle.py
+++ NoIdle.py
@@ -3,12 +3,8 @@ from time import sleep
 
 count = 0
 print("NoIdle 1.1.0")
-print(" ")
-print("---Changelog---")
-print(" - Removed mouse module to make loading faster and have higher compatibility")
-print(" ")
-print("By herobrick360")
-print("To stop macro x out this window!")
+print("By LagTheSystem\n")
+print("To stop macro, close this window!")
 while count == 0:
     sleep(1)
     keyboard.press_and_release('w')
