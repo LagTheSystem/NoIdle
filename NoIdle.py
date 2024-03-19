30c79069e6d88b8c6abddee60c10f0163ac7daa8
LagTheSystem <lagthesystem@duck.com>
2024-03-19 10:43:53 -0500

    Update NoIdle.py

--- NoIdle.py
+++ NoIdle.py
@@ -1,10 +1,21 @@
 import keyboard as kb
 from time import sleep
 
-print("NoIdle 1.3-b1")
+
+enabled = false
+print("NoIdle 1.3-rc1")
 print("By LagTheSystem\n")
 print("To stop macro, close this window!")
 sleep(1)
+def enable():
+    kb.remove_hotkey('f10')
+    kb.add_hotkey('f10', disable)
+    enabled = true
+def disable():
+    enabled = false
+    kb.remove_hotkey('f10')
+    kb.add_hotkey('f10', enable)
+kb.add_hotkey('f10', enable)
 while true:
     kb.KEY_DOWN('W')
     sleep(0.5)
