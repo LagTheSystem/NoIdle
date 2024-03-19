f34e6048e722e3af8fe70e63ee835f3f2d13d1d0
LagTheSystem <lagthesystem@duck.com>
2024-03-19 10:12:42 -0500

    Update NoIdle.py

--- NoIdle.py
+++ NoIdle.py
@@ -2,7 +2,7 @@ import keyboard
 from time import sleep
 
 count = 0
-print("NoIdle 1.1.0")
+print("NoIdle 1.2.0")
 print("By LagTheSystem\n")
 print("To stop macro, close this window!")
 while count == 0:
